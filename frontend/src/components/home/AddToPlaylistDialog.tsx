8160fee8aec20dafa46ce7c90cc25fd41162ad10
TheGauravsahu <itsgauravsahu7@gmail.com>
2025-10-12 06:54:12 +0530

    fix : playlist icon

--- frontend/src/components/home/AddToPlaylistDialog.tsx
+++ frontend/src/components/home/AddToPlaylistDialog.tsx
@@ -18,9 +18,9 @@ const AddToPlaylistDialog = ({ foodId }: { foodId: string }) => {
     return (
       <Dialog>
         <DialogTrigger asChild>
-        <button className="bg-black/50 p-2 rounded-full hover:bg-black/70 cursor-pointer">
-          <ListMusic className="w-6 h-6 text-white" />
-        </button>
+          <button className="bg-black/50 p-2 rounded-full hover:bg-black/70 cursor-pointer">
+            <ListMusic className="w-6 h-6 text-white" />
+          </button>
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
@@ -38,7 +38,9 @@ const AddToPlaylistDialog = ({ foodId }: { foodId: string }) => {
     return (
       <Dialog>
         <DialogTrigger asChild>
-          <button className="btn btn-primary">Add to Playlist</button>
+          <button className="bg-black/50 p-2 rounded-full hover:bg-black/70 cursor-pointer">
+            <ListMusic className="w-6 h-6 text-white" />
+          </button>
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
